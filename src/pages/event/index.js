b34fb4211e3b61739ce9d2f0ee60cc8607bd626e
siam50 <siamurrahman50@gmail.com>
2023-03-13 21:59:51 +0600

    Added Events Card & Modal

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -3,11 +3,13 @@ import Head from "next/head";
 
 const index = () => {
   return (
-    <div>
+    <div className="max-w-[1240px] mx-auto">
       <Head>
         <title>Event</title>
       </Head>
-      <h1>This is Event page</h1>
+      <h1 className=" my-10 text-center font-bold text-3xl">
+        This is Event page
+      </h1>
       <EventView></EventView>
     </div>
   );
