0700d28e9c8e524ad70b0fa965178e2bc8861cd0
siam50 <siamurrahman50@gmail.com>
2023-04-05 17:12:55 +0600

    get users data

---
+++ src/pages/users/index.js
@@ -0,0 +1,12 @@
+import UsersView from "@/features/users/UsersView";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <UsersView></UsersView>
+    </div>
+  );
+};
+
+export default index;
