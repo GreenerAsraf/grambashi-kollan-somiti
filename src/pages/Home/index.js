a49700a241ebd6f5a4cac8e479cfb78a716e5f4e
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-04-15 17:37:57 +0600

    events on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,15 +2,17 @@ import Head from 'next/head';
 import HomePage from '../../features/Home/HomePage';
 import MembersView from '@/features/Members/MembersVIew';
 import Message from '@/features/Message/Message';
+import HomeEvents from '@/features/event/HomeEvents';
 
 const index = () => {
 	return (
-		<div className='max-w-[1240px] mx-auto my-10 '>
+		<div className='max-w-[1250px] mx-auto mb-10 '>
 			<Head>
 				<title>আমরা গ্রামবাসী কল্যাণ সমিতি</title>
 			</Head>
 			<HomePage />
 			<MembersView></MembersView>
+			<HomeEvents></HomeEvents>
 			<Message></Message>
 		</div>
 	);
