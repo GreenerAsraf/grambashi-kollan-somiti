507781611df979eb713630ccd40e7cbab4fa9274
amJayem <csejayem19@gmail.com>
2023-05-02 23:51:47 +0600

    put credit api to store, api is used in debit component

--- src/features/admin/components/dashboard/Debit.js
+++ src/features/admin/components/dashboard/Debit.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import BaseCard from '../baseCard/BaseCard'
 import { Box, Button, TextField } from '@mui/material'
+import {
+  useAddCreditMutation,
+  useAddDebitMutation
+} from '@/slices/api/debitCreditApi'
+import { toast } from 'react-hot-toast'
 
 const Debit = () => {
-  const handleDebit = (e) => {
+  const [addCredit, { isSuccess: creditSuccess }] = useAddCreditMutation()
+  const [addDebit, { isSuccess: debitSuccess }] = useAddDebitMutation()
+
+  const handleCredit = (e) => {
     e.preventDefault()
     const form = e.target
     const credit = form.credit.value
-    console.log(credit)
+    const creditNote = form.creditNote.value
+    const creditInfo = { credit, creditNote }
+    console.log(creditInfo)
+    addCredit(creditInfo)
   }
-  const handleCredit = (e) => {
+  const handleDebit = (e) => {
     e.preventDefault()
     const form = e.target
     const debit = form.debit.value
@@ -19,7 +30,18 @@ const Debit = () => {
       debitNote
     }
     console.log(debitInfo)
+    addDebit(debitInfo)
   }
+
+  // console.log(creditSuccess, debitSuccess)
+
+  if (creditSuccess) {
+    toast.success('Credit Added!')
+  }
+  if (debitSuccess) {
+    toast.success('Debit Added!')
+  }
+
   return (
     <BaseCard title={'Debit and Credit'} variant={'h1'}>
       <Box
@@ -27,7 +49,7 @@ const Debit = () => {
         justifyContent={'center'}
         gap={'5rem'}
         boxShadow={'lg'}>
-        <form onSubmit={handleDebit}>
+        <form onSubmit={handleCredit}>
           <Box
             padding={5}
             height={290}
@@ -39,6 +61,14 @@ const Debit = () => {
               boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)'
             }}>
             <h1 className='text-2xl font-semibold'>Credit</h1>
+
+            <TextField
+              label='Credit Note'
+              variant='standard'
+              name='creditNote'
+              margin='normal'
+              type='text'
+            />
             <TextField
               inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
               type='number'
@@ -51,7 +81,7 @@ const Debit = () => {
           </Box>
         </form>
 
-        <form onSubmit={handleCredit}>
+        <form onSubmit={handleDebit}>
           <Box
             padding={5}
             width={345}
