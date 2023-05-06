41002fdb3457bbbd40da74d0fc48bb8698e685a5
Asraf <asraf.uddin4407@gmail.com>
2023-05-06 11:43:48 +0600

    added gallery

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from 'react'
+import { FaCamera } from 'react-icons/fa'
+import Gallery from 'react-photo-gallery'
 
 const index = () => {
-    return (
-        <div>
-            <h2>This is gallery</h2>
-        </div>
-    );
-};
+  const photos = [
+    {
+      src: 'https://wallpaperaccess.com/full/211836.jpg',
+      width: 200,
+      height: 100,
+      discription: 'This is a images gallery of grambashi kollan somity'
+    },
+    {
+      src: 'https://c4.wallpaperflare.com/wallpaper/482/480/58/beautyful-scenery-wallpaper-preview.jpg',
+      width: 200,
+      height: 100,
+      discription: 'This is a images gallery of grambashi kollan somity'
+    },
+    {
+      src: 'https://wallpaperaccess.com/full/211836.jpg',
+      width: 200,
+      height: 100,
+      discription: 'This is a images gallery of grambashi kollan somity'
+    },
+    {
+      src: 'https://c4.wallpaperflare.com/wallpaper/482/480/58/beautyful-scenery-wallpaper-preview.jpg',
+      width: 200,
+      height: 100,
+      discription: 'This is a images gallery of grambashi kollan somity'
+    },
+    {
+      src: 'https://wallpaperaccess.com/full/211836.jpg',
+      width: 200,
+      height: 100,
+      discription: 'This is a images gallery of grambashi kollan somity'
+    },
+    {
+      src: 'https://c4.wallpaperflare.com/wallpaper/482/480/58/beautyful-scenery-wallpaper-preview.jpg',
+      width: 200,
+      height: 100,
+      discription: 'This is a images gallery of grambashi kollan somity'
+    }
+    // add more images as needed
+  ]
 
-export default index;
+  return (
+    <div>
+      {/* <h2>This is gallery</h2> */}
+
+      <h1>
+        <FaCamera /> গ্রামবাসী কল্যাণ সমিতির স্মৃতিময় ছবি
+      </h1>
+      <Gallery photos={photos} />
+    </div>
+  )
+}
+
+export default index
