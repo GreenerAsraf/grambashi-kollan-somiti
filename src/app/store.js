71caee5391588b170a79b85358414e32951f0c0f
siam50 <siamurrahman50@gmail.com>
2023-05-06 23:44:25 +0600

    add balanceApi midleware

--- src/app/store.js
+++ src/app/store.js
@@ -4,11 +4,13 @@ import { eventApi } from "@/slices/api/eventApi";
 import { userApi } from "@/slices/api/apiSlice";
 import { debitApi } from "@/slices/api/debitCreditApi";
 import { creditApi } from "@/slices/api/creditApi";
+import { balanceApi } from "@/slices/api/balanceApi";
 const { configureStore } = require("@reduxjs/toolkit");
 
 const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
+    [balanceApi.reducerPath]: balanceApi.reducer,
     [eventApi.reducerPath]: eventApi.reducer,
     [debitApi.reducerPath]: debitApi.reducer,
     [creditApi.reducerPath]: creditApi.reducer,
@@ -18,6 +20,7 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(userApi.middleware)
+      .concat(balanceApi.middleware)
       .concat(eventApi.middleware)
       .concat(debitApi.middleware)
       .concat(creditApi.middleware),
