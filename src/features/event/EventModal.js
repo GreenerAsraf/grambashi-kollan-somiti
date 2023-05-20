dd0c7d37f961c979e5be84eae8f613885fd14d3c
amJayem <csejayem19@gmail.com>
2023-05-20 23:24:40 +0600

    alt added to image

--- src/features/event/EventModal.js
+++ src/features/event/EventModal.js
@@ -1,26 +1,30 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image'
+import React from 'react'
 
 const EventModal = ({ eventData }) => {
   return (
     <div>
       {/* Put this part before </body> tag */}
-      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box relative">
+      <input type='checkbox' id='my-modal-3' className='modal-toggle' />
+      <div className='modal'>
+        <div className='modal-box relative'>
           <label
-            htmlFor="my-modal-3"
-            className="btn btn-sm btn-circle absolute right-2 top-2"
-          >
+            htmlFor='my-modal-3'
+            className='btn btn-sm btn-circle absolute right-2 top-2'>
             ✕
           </label>
-          <Image src={eventData?.image} width={600} height={600} alt="/" />
-          <h3 className="text-lg font-bold mt-4">{eventData?.eventName}</h3>
-          <p className="py-4">{eventData?.description}</p>
+          <Image
+            src={eventData?.image}
+            width={600}
+            height={600}
+            alt={eventData?.eventName}
+          />
+          <h3 className='text-lg font-bold mt-4'>{eventData?.eventName}</h3>
+          <p className='py-4'>{eventData?.description}</p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default EventModal;
+export default EventModal
