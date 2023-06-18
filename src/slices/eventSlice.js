37fe53443d570dd9f4f2d7d62e687ccfe212eced
amJayem <csejayem19@gmail.com>
2023-06-18 23:48:13 +0600

    port updated

--- src/slices/eventSlice.js
+++ src/slices/eventSlice.js
@@ -1,57 +1,57 @@
-import axios from "axios";
+import axios from 'axios'
 
-const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit')
 
 // Get Events data
-export const fetchEvents = createAsyncThunk("events/fetchEvents", async () => {
-  const res = await axios.get("events.json");
-  return res.data;
-});
+export const fetchEvents = createAsyncThunk('events/fetchEvents', async () => {
+  const res = await axios.get('events.json')
+  return res.data
+})
 // Add Event data
 export const addEvent = createAsyncThunk(
-  "events/addEvent",
+  'events/addEvent',
   async (eventData) => {
-    const res = await axios.post("http://localhost:5000/add-event", eventData);
-    return res;
+    const res = await axios.post('http://localhost:8000/add-event', eventData)
+    return res
   }
-);
+)
 
 const eventSlice = createSlice({
-  name: "events",
+  name: 'events',
   initialState: {
     isLoading: false,
     postSuccess: false,
     events: [],
-    error: null,
+    error: null
   },
   extraReducers: (builder) => {
     builder.addCase(fetchEvents.pending, (state) => {
-      state.isLoading = true;
-    });
+      state.isLoading = true
+    })
     builder.addCase(fetchEvents.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.events = action.payload;
-      state.error = null;
-    });
+      state.isLoading = false
+      state.events = action.payload
+      state.error = null
+    })
     builder.addCase(fetchEvents.rejected, (state, action) => {
-      state.isLoading = false;
-      state.events = [];
-      state.error = action.error.message;
-    });
+      state.isLoading = false
+      state.events = []
+      state.error = action.error.message
+    })
     builder.addCase(addEvent.pending, (state) => {
-      state.isLoading = true;
-    });
+      state.isLoading = true
+    })
     builder.addCase(addEvent.fulfilled, (state) => {
-      state.isLoading = false;
-      state.postSuccess = true;
-    });
+      state.isLoading = false
+      state.postSuccess = true
+    })
     builder.addCase(addEvent.rejected, (state, action) => {
-      state.isLoading = false;
-      state.postSuccess = false;
-      state.events = [];
-      state.error = action.error.message;
-    });
-  },
-});
+      state.isLoading = false
+      state.postSuccess = false
+      state.events = []
+      state.error = action.error.message
+    })
+  }
+})
 
-export default eventSlice.reducer;
+export default eventSlice.reducer
