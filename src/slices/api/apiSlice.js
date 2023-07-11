269b0e49951ff34e4f9cf2841e9ce3298458f598
amJayem <csejayem19@gmail.com>
2023-07-11 21:59:50 +0600

    localhost replaced with vercel link

--- src/slices/api/apiSlice.js
+++ src/slices/api/apiSlice.js
@@ -1,37 +1,38 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
-	reducerPath: 'userApi',
-	baseQuery: fetchBaseQuery({
-		// baseUrl: "users.json",
-		baseUrl: 'http://localhost:8000',
-		// 'https://grambasi-kollyan-somiti-server.vercel.app'
-	}),
-	tagTypes: ['Users'],
-	endpoints: (builder) => ({
-		getUsers: builder.query({
-			query: () => ({
-				url: '/all-users',
-			}),
-			providesTags: ['Users'],
-		}),
-		addUser: builder.mutation({
-			query: (data) => ({
-				url: '/add-user',
-				method: 'POST',
-				body: data,
-			}),
-			invalidatesTags: ['Users'],
-		}),
-		deleteUser: builder.mutation({
-			query: (id) => ({
-				url: `/delete-user?id=${id}`,
-				method: 'delete',
-			}),
-			invalidatesTags: ['Users'],
-		}),
-	}),
-});
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({
+    // baseUrl: "users.json",
+    baseUrl:
+      // 'http://localhost:8000',
+      'https://grambasi-kollyan-somiti-server.vercel.app'
+  }),
+  tagTypes: ['Users'],
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => ({
+        url: '/all-users'
+      }),
+      providesTags: ['Users']
+    }),
+    addUser: builder.mutation({
+      query: (data) => ({
+        url: '/add-user',
+        method: 'POST',
+        body: data
+      }),
+      invalidatesTags: ['Users']
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/delete-user?id=${id}`,
+        method: 'delete'
+      }),
+      invalidatesTags: ['Users']
+    })
+  })
+})
 
 export const { useGetUsersQuery, useAddUserMutation, useDeleteUserMutation } =
-	userApi;
+  userApi
