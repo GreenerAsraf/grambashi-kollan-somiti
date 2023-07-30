9f98277bd0b835d5248944275cce85037f583bb6
Asraf <asraf.uddin4407@gmail.com>
2023-07-31 00:35:47 +0600

    user view in homepage modified

--- src/features/users/UsersView.js
+++ src/features/users/UsersView.js
@@ -34,11 +34,14 @@ const UsersView = ({ searchUser }) => {
             <div className='flex flex-col space-y-4'>
               <div>
                 <h2 className='text-2xl font-semibold'>{user.name}</h2>
-                <span className='text-sm text-black'>General manager</span>
+                <span className='text-sm text-black'>{user.role}</span>
+                <br />
+                <span className='text-sm text-black'>{user.memberRule}</span>
               </div>
               <div className='space-y-1'>
                 <span className='flex items-center space-x-2'>
-                  <svg
+                  Member Id:
+                  {/* <svg
                     xmlns='http://www.w3.org/2000/svg'
                     viewBox='0 0 512 512'
                     aria-label='Email address'
@@ -46,8 +49,8 @@ const UsersView = ({ searchUser }) => {
                     <path
                       fill='currentColor'
                       d='M274.6,25.623a32.006,32.006,0,0,0-37.2,0L16,183.766V496H496V183.766ZM464,402.693,339.97,322.96,464,226.492ZM256,51.662,454.429,193.4,311.434,304.615,256,268.979l-55.434,35.636L57.571,193.4ZM48,226.492,172.03,322.96,48,402.693ZM464,464H48V440.735L256,307.021,464,440.735Z'></path>
-                  </svg>
-                  <span className='text-black'>leroy.jenkins@company.com</span>
+                  </svg> */}
+                  <span className='text-black'>{user.memberId}</span>
                 </span>
                 <span className='flex items-center space-x-2'>
                   <svg
