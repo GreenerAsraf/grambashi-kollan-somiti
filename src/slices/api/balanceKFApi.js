c902ee40d3f8e027ae10155b2f32c17f2e133289
amJayem <csejayem19@gmail.com>
2023-07-30 18:44:55 +0600

    vercel api connected

--- src/slices/api/balanceKFApi.js
+++ src/slices/api/balanceKFApi.js
@@ -5,8 +5,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const balanceKFApi = createApi({
   reducerPath: 'balanceKFApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8000'
-    // 'https://grambasi-kollyan-somiti-server.vercel.app'
+    baseUrl:
+      // 'http://localhost:8000'
+      'https://grambasi-kollyan-somiti-server.vercel.app'
   }),
   tagTypes: ['BalanceKF'],
   endpoints: (builder) => ({
