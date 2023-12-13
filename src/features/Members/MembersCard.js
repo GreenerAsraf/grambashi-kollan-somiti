337cdccb6a64c3cb8d09eb6269b4d8e7cb5a2729
amjayem <csejayem19@gmail.com>
2023-12-13 23:27:45 +0600

    modified display data

--- src/features/Members/MembersCard.js
+++ src/features/Members/MembersCard.js
@@ -1,75 +1,69 @@
-import React from "react";
-import Image from "next/image";
-import { Card, CardContent, Typography, Button, Grid } from "@mui/material";
+import React from 'react'
+import Image from 'next/image'
+import { Card, CardContent, Typography, Button, Grid } from '@mui/material'
 
 const MembersCard = ({ member }) => {
   const handleModal = (member) => {
-    setMember(member);
-  };
+    setMember(member)
+  }
 
   return (
-    <Grid container className="flex justify-center">
+    <Grid container className='flex justify-center'>
       <Grid
         item
         xs={12}
         lg={10}
         sx={{
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
+          display: 'flex',
+          alignItems: 'center'
+        }}>
         <Card
           sx={{
             p: 0,
-            width: "100%",
-          }}
-        >
+            width: '100%'
+          }}>
           <Image
-            className="w-[250px] h-[250px] rounded-full p-5 mx-auto"
+            className='w-[250px] h-[250px] rounded-full p-5 mx-auto'
             src={member.image}
-            width={"600"}
-            height={"200"}
+            width={'600'}
+            height={'200'}
             alt={member.title}
           />
           {/* </figure> */}
           <CardContent
             sx={{
-              paddingLeft: "30px",
-              paddingRight: "30px",
-            }}
-          >
+              paddingLeft: '30px',
+              paddingRight: '30px'
+            }}>
             <Typography
               sx={{
-                fontSize: "24px",
-                fontWeight: "600",
-              }}
-            >
+                fontSize: '24px',
+                fontWeight: '600'
+              }}>
               {member.name}
             </Typography>
-            <Typography>{member.memberRule}</Typography>
+            <Typography>{member.memberRole.role}</Typography>
             <span>Total Balance: </span>
             <p
               className={`inline text-start text-lg py-2 ${
-                member?.totalBalance === 0 ? "text-red-300" : "text-green-300"
-              }`}
-            >
+                member?.totalBalance === 0 ? 'text-red-300' : 'text-green-300'
+              }`}>
               {member?.totalBalance}
             </p>
             <Typography
-              color="textSecondary"
+              color='textSecondary'
               sx={{
-                fontSize: "16px",
-                fontWeight: "600",
-                mt: 1,
-              }}
-            >
+                fontSize: '16px',
+                fontWeight: '600',
+                mt: 1
+              }}>
               {member.title}
             </Typography>
           </CardContent>
         </Card>
       </Grid>
     </Grid>
-  );
-};
+  )
+}
 
-export default MembersCard;
+export default MembersCard
--- src/features/Members/MembersVIew.js
+++ src/features/Members/MembersVIew.js
@@ -1,132 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMembers } from '../../slices/membersSlice';
-import MembersCard from './MembersCard';
-import { useGetUsersQuery } from '@/slices/api/apiSlice';
-import { Box, Stack } from '@mui/material';
-import { useGetBalanceQuery } from '@/slices/api/balanceApi';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchMembers } from '../../slices/membersSlice'
+import MembersCard from './MembersCard'
+import { useGetUsersQuery } from '@/slices/api/apiSlice'
+import { Box, Stack } from '@mui/material'
+import { useGetBalanceQuery } from '@/slices/api/balanceApi'
 
 const MembersView = () => {
-	const [visible, setVisible] = useState(3);
-	const [btn, setBtn] = useState(false);
-	const { isLoading, members, error } = useSelector(
-		(state) => state.membersReducer
-	);
-	const dispatch = useDispatch();
-	const { data } = useGetUsersQuery();
+  const [visible, setVisible] = useState(3)
+  const [btn, setBtn] = useState(false)
+  const { isLoading, members, error } = useSelector(
+    (state) => state.membersReducer
+  )
+  const dispatch = useDispatch()
+  const { data } = useGetUsersQuery()
 
-	const { data: balanceQuery } = useGetBalanceQuery();
-	const balanceData = balanceQuery?.result;
+  const { data: balanceQuery } = useGetBalanceQuery()
+  const balanceData = balanceQuery?.result
 
-	// Create an object to store the data and summation for all amount of a user
-	const balance = {};
-	// Iterate over the balanceData array
-	if (balanceData) {
-		for (const data of balanceData) {
-			const memberId = data.memberId;
-			const amount = data.amount;
+  // Create an object to store the data and summation for all amount of a user
+  const balance = {}
+  // Iterate over the balanceData array
+  if (balanceData) {
+    for (const data of balanceData) {
+      const memberId = data.memberId
+      const amount = data.amount
 
-			// Check if the memberId already exists in the balance object
-			if (balance[memberId]) {
-				// If it exists, add the amount to the existing total
-				balance[memberId].amount += amount;
-			} else {
-				// If it doesn't exist, initialize the object with the data
-				balance[memberId] = {
-					memberName: data.memberName,
-					amount: amount,
-					memberId: memberId,
-				};
-			}
-		}
-	}
+      // Check if the memberId already exists in the balance object
+      if (balance[memberId]) {
+        // If it exists, add the amount to the existing total
+        balance[memberId].amount += amount
+      } else {
+        // If it doesn't exist, initialize the object with the data
+        balance[memberId] = {
+          memberName: data.memberName,
+          amount: amount,
+          memberId: memberId
+        }
+      }
+    }
+  }
 
-	// converting object to array
-	const balanceArray = Object.values(balance);
-	// console.log('balanceArray: ', balanceArray)
-	// console.log('data: ', data)
+  // converting object to array
+  const balanceArray = Object.values(balance)
+  // console.log('balanceArray: ', balanceArray)
+  // console.log('data: ', data)
 
-	const updatedData = data?.map((member) => {
-		const balance = balanceArray?.find(
-			(balanceMember) => balanceMember.memberId === +member.memberId
-		);
-		if (balance) {
-			// console.log(balance)
-			return {
-				...member,
-				totalBalance: balance.amount,
-			};
-		} else
-			return {
-				...member,
-				totalBalance: 0,
-			};
-	});
+  const updatedData = data?.map((member) => {
+    const balance = balanceArray?.find(
+      (balanceMember) => balanceMember.memberId === +member.memberId
+    )
+    if (balance) {
+      // console.log(balance)
+      return {
+        ...member,
+        totalBalance: balance.amount
+      }
+    } else
+      return {
+        ...member,
+        totalBalance: 0
+      }
+  })
 
-	useEffect(() => {
-		dispatch(fetchMembers());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchMembers())
+  }, [dispatch])
 
-	const showAll = () => {
-		const visible = members.length;
-		setVisible(visible);
-		setBtn(true);
-	};
+  const showAll = () => {
+    const visible = members.length
+    setVisible(visible)
+    setBtn(true)
+  }
+  const sortedDataMemberRole = updatedData?.sort(
+    (a, b) => a?.memberRole?.id - b?.memberRole?.id
+  )
 
-	return (
-		<div className='text-center'>
-			{isLoading && <h1 className=' text-xl font-bold'>Loading..........</h1>}
-			{error && <h1 className=' text-xl font-bold'>{error}</h1>}
-			{/* কার্যকরী কমিটির সদস্যবৃন্দ */}
-			<Stack gap={5}>
-				<Box>
-					<h2 className='text-start font-semibold text-2xl mb-3'>
-						<u>কার্যকরী কমিটির সদস্যবৃন্দ:-</u>
-					</h2>
-					{updatedData && (
-						<div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 items-center '>
-							{updatedData
-								.filter((f) => f.role === 'কার্যকরী কমিটি')
-								.map((member) => (
-									<div key={member.id}>
-										<MembersCard member={member}></MembersCard>
-									</div>
-								))}
-						</div>
-					)}
-				</Box>
-				{/* উপদেষ্টা কমিটির সদস্যবৃন্দ */}
-				<Box>
-					<h2 className='text-start font-semibold text-2xl mb-3'>
-						<u>উপদেষ্টা কমিটির সদস্যবৃন্দ:-</u>
-					</h2>
-					{updatedData && (
-						<div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 items-center '>
-							{updatedData
-								.filter(
-									(f) =>
-										// condition need to change
-										f.role === 'উপদেষ্টা কমিটি'
-								)
-								.map((member) => (
-									<div key={member.id}>
-										<MembersCard member={member}></MembersCard>
-									</div>
-								))}
-						</div>
-					)}
-				</Box>
-			</Stack>
+  // console.log('updatedData: ', updatedData)
+  // console.log('sortedDataMemberRole: ', sortedDataMemberRole)
+  return (
+    <div className='text-center'>
+      {isLoading && <h1 className=' text-xl font-bold'>Loading..........</h1>}
+      {error && <h1 className=' text-xl font-bold'>{error}</h1>}
+      {/* কার্যকরী কমিটির সদস্যবৃন্দ */}
+      <Stack gap={5}>
+        <Box>
+          <h2 className='text-start font-semibold text-2xl mb-3'>
+            <u>কার্যকরী কমিটির সদস্যবৃন্দ:-</u>
+          </h2>
+          {updatedData && (
+            <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 items-center '>
+              {sortedDataMemberRole
+                .filter((f) => f?.role?.role === 'কার্যকরী কমিটি')
+                .map((member) => (
+                  <div key={member.id}>
+                    <MembersCard member={member}></MembersCard>
+                  </div>
+                ))}
+            </div>
+          )}
+        </Box>
+        {/* উপদেষ্টা কমিটির সদস্যবৃন্দ */}
+        <Box>
+          <h2 className='text-start font-semibold text-2xl mb-3'>
+            <u>উপদেষ্টা কমিটির সদস্যবৃন্দ:-</u>
+          </h2>
+          {updatedData && (
+            <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 items-center '>
+              {updatedData
+                .filter(
+                  (f) =>
+                    // condition need to change
+                    f?.role?.role === 'উপদেষ্টা কমিটি'
+                )
+                .map((member) => (
+                  <div key={member.id}>
+                    <MembersCard member={member}></MembersCard>
+                  </div>
+                ))}
+            </div>
+          )}
+        </Box>
+      </Stack>
 
-			{/* <button
+      {/* <button
         onClick={showAll}
         className="mt-10 btn btn-primary"
         disabled={btn}
       >
         Show All
       </button> */}
-		</div>
-	);
-};
+    </div>
+  )
+}
 
-export default MembersView;
+export default MembersView
