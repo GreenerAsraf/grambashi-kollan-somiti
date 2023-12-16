099ceefa6d8f55e727b52bfc431aa48fe79141ad
amjayem <csejayem19@gmail.com>
2023-12-17 00:03:25 +0600

    data shown and pagination modified

--- src/features/admin/components/dashboard/AllUsers/Pagination.js
+++ src/features/admin/components/dashboard/AllUsers/Pagination.js
@@ -1,37 +1,48 @@
-import React, { useState } from 'react';
-import { HiChevronDoubleRight, HiChevronDoubleLeft } from 'react-icons/hi2';
+import { useState } from 'react'
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi2'
 
-const Pagination = ({ setPage, searchUser, page }) => {
-  const [pre, setPre] = useState(0);
-  const [next, setNext] = useState(10);
-  const userLength = searchUser?.length;
+const Pagination = ({
+  setPage,
+  searchUser,
+  page,
+  totalCount,
+  setPageSize,
+  pageSize
+}) => {
+  const [pre, setPre] = useState(0)
+  const [next, setNext] = useState(10)
+  const userLength = searchUser?.length
   // console.log(userLength);
 
-  let lnt = 0;
-  lnt = parseInt(Math.ceil(userLength / 9));
+  let lnt = 0
+  lnt = parseInt(Math.ceil(userLength / 9))
 
-  const arr = [];
+  const arr = []
   for (let i = 1; i <= lnt; i++) {
-    arr.push(i);
+    arr.push(i)
   }
   const pagination = (e) => {
-    setPage(e);
-  };
+    setPage(e)
+    // setPageSize((p) => p + 10)
+  }
   const prevBtn = () => {
     if (pre > 0) {
-      setPre(pre - 1);
-      setNext(next - 1);
+      setPre(pre - 1)
+      setNext(next - 1)
     }
-  };
+  }
   const nextBtn = () => {
-    setPre(pre + 1);
-    setNext(next + 1);
-  };
+    setPre(pre + 1)
+    setNext(next + 1)
+    setPageSize((p) => p + 10)
+
+    setPage((p) => p + 1)
+  }
 
   return (
     <div className='btn-group gap-3 flex justify-center mt-9'>
       <button
-        className=' btn-outline btn-success'
+        className=' btn  btn-success btn-circle btn-outline '
         onClick={() => prevBtn()}>
         <HiChevronDoubleLeft />
       </button>
@@ -46,12 +57,16 @@ const Pagination = ({ setPage, searchUser, page }) => {
       ))}
       {/* <HiArrowRightCircle onClick={() => nextBtn()} /> */}
       <button
-        className={next === lnt ? 'btn-disabled' : 'btn-outline btn-success'}
-        onClick={() => nextBtn()}>
-        <HiChevronDoubleRight />
+        className={
+          totalCount > pageSize
+            ? 'btn btn-success btn-circle btn-outline'
+            : 'btn btn-disabled btn-circle  btn-outline'
+        }
+        onClick={() => totalCount > pageSize && nextBtn()}>
+        <HiChevronDoubleRight color='black' />
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
